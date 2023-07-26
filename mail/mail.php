c5563ca1582db9e6b63c27c1d06d5cc08907de00
khw0512 <hwunderstand@gmail.com>
2023-07-26 10:27:34 +0900

    0726-02

--- mail/mail.php
+++ mail/mail.php
@@ -1,17 +1,17 @@
 <?php
 parse_str($_POST['form_data'], $form);
 
-define('TO_EMAIL', 'info@realdatalabs.co.kr');
-define('SUBJECT', 'RDLS 홈페이지');
+define('TO_EMAIL', 'mylves@hanmail.net');
+define('SUBJECT', 'Portfolio');
 define('FROM_EMAIL', $form['con_email']);
 
 $MESSAGE = '안녕하세요 담당자님, <br/><br/>';
-$MESSAGE .= 'Realdatalabs 홈페이지를 통해 아래와 같은 메시지가 접수되었습니다 : <br/><br/>';
-$MESSAGE .= 'Name : '.$form['con_name'].'<br/>';
-$MESSAGE .= 'Email : '.$form['con_email'].'<br/>';
-$MESSAGE .= 'Subject : '.$form['con_subject'].'<br/>';
+$MESSAGE .= 'Portfolio를통해 아래와 같은 메시지가 접수되었습니다 : <br/><br/>';
+$MESSAGE .= 'Name : '.$form['your-name'].'<br/>';
+$MESSAGE .= 'Email : '.$form['your-email'].'<br/>';
+$MESSAGE .= 'Subject : '.$form['your-subject'].'<br/>';
 
-$MESSAGE .= 'Message : <br/>'.$form['con_message'].'<br/><br/>';
+$MESSAGE .= 'Message : <br/>'.$form['your-message'].'<br/><br/>';
 $MESSAGE .= 'Regards';
 
 $HEADERS = "MIME-Version: 1.0" . "\r\n";
